c2f92b6b17f4c7be859de16bd368acc4e7a50a7a
matthew-c-atu <148288884+matthew-c-atu@users.noreply.github.com>
2024-04-07 16:16:21 +0100

    test GET request of audio data from backend server

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import Transport from './Transport.jsx'
+
+// import Transport from './Transport.jsx'
 
 const playerLogo = "https://image.similarpng.com/very-thumbnail/2020/12/Popular-Music-icon-in-round-black-color-on-transparent-background-PNG.png" 
 
@@ -16,12 +14,14 @@ export default function App() {
           <img src={playerLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
+      <div>
+        <button className="logo" onClick={GetAudioData}>
+          <h4>Get audio</h4>
+        </button>
+        <audio id="player" src="data:audio/x-wav;base64"></audio>
+      </div>
       <h1>Music Player</h1>
       <HelloText />
-      <Transport />
-      <div>
-        <audio id="player" src="data:audio/x-wav;base64,UklGRvC…"></audio>
-      </div>
     </>
   )
 }
@@ -30,4 +30,13 @@ function HelloText() {
   return (
     <h1>Hello</h1> 
   )
+}
+
+function GetAudioData() {
+ console.log("foo")
+  fetch( "http://localhost:8080",
+    {
+      method: "GET"
+    }
+  ).then((response) => console.log(response))
 }
