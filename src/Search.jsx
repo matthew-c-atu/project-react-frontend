271c58c85e1bba7f421ca2a268969da6d8f4e0bb
matthew-c-atu <148288884+matthew-c-atu@users.noreply.github.com>
2024-04-23 08:41:52 +0100

    initial searchbox commit

---
+++ src/Search.jsx
@@ -0,0 +1,16 @@
+import './Search.css'
+import {useState} from 'react'
+
+export default function Search() {
+  
+  return (
+    <>
+      <div className="searchBg">
+        <div className="grid-container">
+          <input type="text"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
